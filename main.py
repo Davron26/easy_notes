4df468905d797114e6fabfce30ddc6ecf224710a
Unknown <davron.tokhirov@gmail.com>
2020-08-17 22:56:50 +0500

    widget do not display

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.button import ButtonBehavior, Button
+from kivy.uix.image import Image
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
+from kivy.core.window import Window
+
+#Window.clearcolor = (1, 1, 1, 1)
 
 class ListScreen(Screen):
     pass
@@ -8,11 +13,24 @@ class NotesScreen(Screen):
     pass
 class NoteScreen(Screen):
     pass
+class ImageButton(ButtonBehavior, Image):
+    pass
 
 GUI = Builder.load_file("note.kv")
 
 class NoteApp(App):
+    def change_screen(self, screen_name):
+        tr = NoTransition()
+        screen_manager = self.root.ids["screen_manager"]
+        screen_manager.transition = tr
+        screen_manager.current = screen_name
+    def create_list(self):
+        self.gl.add_widget(Button())
+    def setup(self):
+        self.list = self.root.ids["list_screen"]
+        self.gl = self.list.ids["fl"]
     def build(self):
+        self.setup()
         return GUI
 
 if __name__ == '__main__':
