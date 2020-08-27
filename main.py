f18957b701f24ca9a9e63a793ca29646ab3ab7d4
Unknown <davron.tokhirov@gmail.com>
2020-08-27 15:58:21 +0500

    android permission error

--- main.py
+++ main.py
@@ -2,16 +2,21 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.button import ButtonBehavior, Button
 from kivy.uix.image import Image
-from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
+from kivy.uix.screenmanager import Screen, NoTransition
 from kivy.core.window import Window
+from kivy.storage.jsonstore import JsonStore
+from android.permissions import request_permissions, Permission
+request_permissions([Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE])
 
-#Window.clearcolor = (1, 1, 1, 1)
+Window.clearcolor = (1, 1, 1, 1)
 
 class ListScreen(Screen):
     pass
 class NotesScreen(Screen):
     pass
 class NoteScreen(Screen):
+    pass
+class CreateList(Screen):
     pass
 class ImageButton(ButtonBehavior, Image):
     pass
@@ -21,14 +26,27 @@ GUI = Builder.load_file("note.kv")
 class NoteApp(App):
     def change_screen(self, screen_name):
         tr = NoTransition()
-        screen_manager = self.root.ids["screen_manager"]
+        screen_manager = GUI.ids["screen_manager"]
         screen_manager.transition = tr
         screen_manager.current = screen_name
     def create_list(self):
-        self.gl.add_widget(Button())
+        if len(self.text_input.text) > 37:
+            self.name_long.text = "Имя нового списка слишком длинное!!!"
+        else:
+            self.gl.add_widget(Button(size_hint_y=None, height=100, text=self.text_input.text,
+                                      on_release=self.test()))
+            self.text_input.text = "Напишите имя сюда"
+            self.name_long.text = ""
+            self.change_screen("list_screen")
+    def test(self):
+        test = JsonStore('test.json')
+        test.put('tito', name='Mathieu', org='kivy')
     def setup(self):
-        self.list = self.root.ids["list_screen"]
-        self.gl = self.list.ids["gl"]
+        self.list_screen = GUI.ids["list_screen"]
+        self.create_list_screen = GUI.ids["create_list"]
+        self.gl = self.list_screen.ids["gl"]
+        self.text_input = self.create_list_screen.ids["text_input"]
+        self.name_long = self.create_list_screen.ids["name_long"]
     def build(self):
         self.setup()
         return GUI
